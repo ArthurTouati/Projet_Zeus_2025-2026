a42405e6a515166c7b2677fc6aa337dad1430efa
Arthur Touati <134126573+ArthurTouati@users.noreply.github.com>
2025-11-10 15:20:47 +0100

    Integrate GPSHandler for GPS data collection
    Added GPSHandler integration to read and export GPS data.

--- Module Data/main_data.py
+++ Module Data/main_data.py
@@ -1,5 +1,6 @@
 import time
 import IMUHandler as Imu
+import GPSHandler as Gps
 import board
 import busio
 import DataHandler as Data
@@ -8,6 +9,7 @@ from datetime import datetime
 # Setup
 i2c = busio.I2C(board.SCL,board.SDA)
 imu = Imu.IMUHandler(i2c)
+gps = Gps.GPSHandler() 
 i = 0
 header_time = ['gt','rt']
 time_dict = dict.fromkeys(header_time)
@@ -16,21 +18,17 @@ ground_time, rocket_time = [],[]
 while i <= 10:
     time.sleep(0.5)
     imu.read_data()
+    gps.read_data()
     ground_time.append(datetime.now().strftime('%H:%M:%S'))
     rocket_time.append(i)
     i+= 0.5
 
 imu_dict = imu.export_to_dict()
+gps_dict = gps.export_to_dict()
 time_dict['gt'] = ground_time
 time_dict['rt'] = rocket_time
 print(time_dict)
 print(imu_dict)
-header_gps = ['x','y','z']
-gps_dict = dict.fromkeys(header_gps)
-gt = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-
-gps_dict['x'] = gt
-gps_dict['y'] = gt
-gps_dict['z'] = gt
+print(gps_dict)
 trajectory_file = Data.TrajectoryData()
 trajectory_file.export_data(time_dict,gps_dict,imu_dict)
