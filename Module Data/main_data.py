cd88efcdc539b1717c294801fbaee322f85cfb1e
ArthurTouati <arthurtouati92@gmail.com>
2025-11-16 09:22:46 +0100

    - Add `ESP32Handler` and `ADCHandler` modules for handling ESP32 communication and ADC sensor data. - Update `main_data.py` to integrate ESP32 and ADC handlers into rocket parameter logging. - Update `RocketData` export logic to include ESP32 and battery data. - Add test script `Power change/ADC_converter.py` for ADC functionality. - Simplify I2C error handling and buffer logic in `master.py`. - Update Python SDK version in project configuration to `3.11`.

--- Module Data/main_data.py
+++ Module Data/main_data.py
@@ -1,42 +1,9 @@
 import time
 import board
 import busio
-from datetime import datetime
-import IMUHandler as Imu
-import GPSHandler as Gps
 import DataHandler as Data
 import ESP32Handler as Esp32
 import ADCHandler as Adc
-
-""" Code trajectoire : 
-# Setup
-i2c = busio.I2C(board.SCL,board.SDA)
-imu = Imu.IMUHandler(i2c)
-gps = Gps.GPSHandler()
-esp32 = Esp32.ESP32Handler(i2c)
-i = 0
-header_time = ['gt','rt']
-time_dict = dict.fromkeys(header_time)
-ground_time, rocket_time = [],[]
-
-while i <= 10:
-    time.sleep(0.5)
-    imu.read_data()
-    gps.read_data()
-    ground_time.append(datetime.now().strftime('%H:%M:%S'))
-    rocket_time.append(i)
-    i+= 0.5
-
-imu_dict = imu.export_to_dict()
-gps_dict = gps.export_to_dict()
-time_dict['gt'] = ground_time
-time_dict['rt'] = rocket_time
-print(time_dict)
-print(imu_dict)
-print(gps_dict)
-trajectory_file = Data.TrajectoryData()
-trajectory_file.export_data(time_dict,gps_dict,imu_dict)
-"""
 
 """ Code fusée : """
 # Setup :
